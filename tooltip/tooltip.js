0f4e8b67b6641890100c0a7efb33f81dbb0f8c0f
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2023-11-19 23:38:11 +0100

    Shadow DOM, first approach. Still very rude, but begins to be usable (slots included)

--- tooltip/tooltip.js
+++ tooltip/tooltip.js
@@ -3,16 +3,18 @@ class Tooltip extends HTMLElement {
     super();
     this._tooltipContainer;
     this._tooltipText = "Default text";
+    this.attachShadow({ mode: "open" });
+    const template = document.querySelector("#vic-tooltip-template");
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 
   connectedCallback() {
     const textAttribute = this.getAttribute("text");
     if (textAttribute) this._tooltipText = textAttribute;
-    const tooltipIcon = document.createElement("span");
-    tooltipIcon.textContent = "(**) ";
+    const tooltipIcon = this.shadowRoot.querySelector("span");
     tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
     tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
-    this.appendChild(tooltipIcon);
+    this.shadowRoot.appendChild(tooltipIcon);
   }
 
   _showTooltip() {
@@ -22,11 +24,11 @@ class Tooltip extends HTMLElement {
     this._tooltipContainer.style.color = "white";
     this._tooltipContainer.style.position = "absolute";
     this._tooltipContainer.style.padding = "6px 10px";
-    this.appendChild(this._tooltipContainer);
+    this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
   _hideTooltip() {
-    this.removeChild(this._tooltipContainer);
+    this.shadowRoot.removeChild(this._tooltipContainer);
   }
 }
 
