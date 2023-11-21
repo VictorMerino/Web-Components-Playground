ee0caaecd9e56ab20f125be00417c03947a0795c
Víctor Merino <5514514+VictorMerino@users.noreply.github.com>
2023-11-21 21:34:09 +0100

    Modal with more complex code: this will let us make comprobations in forms and stuff

---
+++ modal/modal.js
@@ -0,0 +1,112 @@
+class Modal extends HTMLElement {
+  constructor() {
+    super()
+    this.attachShadow({mode: 'open'})
+    this._isModalOpen = false;
+    this.shadowRoot.innerHTML = /*html*/ `
+      <style>
+        #backdrop {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100vh;
+          background: rgba(0, 0, 0, 0.75);
+          z-index: 10;
+        }
+        #modal {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          z-index: 11;
+          position: fixed;
+          top: 15vh;
+          left: 25%;
+          padding: 2rem 3rem;
+          width: 40%;
+          min-height: 20rem;
+          background-color: white;
+          border-radius: 1rem;
+          box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+        }
+
+        .hidden { display: none!important }
+
+        .modal-header {}
+        .modal-main {}
+
+        .modal-footer {
+          border-top: 1px solid #ececec;
+          display: flex;
+          justify-content: flex-end;
+          gap: 1rem;
+          padding-top: 1rem
+        }
+
+        button {
+          padding: 6px 10px;
+          border-radius: .25rem;
+          border-color: #ececec;
+        }
+        .button-primary {
+          background-color: var(--color-button-primary);
+          color: white
+        }
+      </style>
+      <div id="modal" class="hidden">
+        <section class="modal-header">
+          <h3>Header</h3>
+        </section>
+        <section class="modal-main">
+          <slot>Modal content</slot>
+        </section>
+        <section class="modal-footer">
+          <button id="cancel">Cancel</button>
+          <button id="confirm" class="button-primary">Confirm</button>
+      </div>
+      <div id="backdrop" class="hidden"></div>
+    `
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) return
+    console.log({name, newValue})
+    if (name === 'open') {
+      if (this.hasAttribute('open')) {
+        console.log(document.querySelector('#backdrop'))
+        this.shadowRoot.querySelector('#backdrop').classList.remove('hidden')
+        this.shadowRoot.querySelector('#modal').classList.remove('hidden')
+      }
+      else {
+        this.shadowRoot.querySelector('#backdrop').classList.add('hidden')
+        this.shadowRoot.querySelector('#modal').classList.add('hidden')
+      }
+    }
+  }
+
+  static get observedAttributes() {
+    return ['open']
+  }
+
+  connectedCallback() {
+    this.shadowRoot.querySelector('#confirm', this._confirm.bind(this))
+    this.shadowRoot.querySelector('#confirm').addEventListener('click', this._confirm.bind(this))
+    this.shadowRoot.querySelector('#cancel').addEventListener('click', this._cancel.bind(this))
+  }
+
+  _confirm() {
+    this._close()
+  }
+
+  _cancel() {
+    this._close()
+  }
+
+  _close() {
+    this.shadowRoot.host.removeAttribute('open')
+  }
+
+
+}
+
+customElements.define('vic-modal', Modal)
