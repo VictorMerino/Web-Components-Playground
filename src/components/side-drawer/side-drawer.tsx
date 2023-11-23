cbd79f536c0301ac5b3a4d2686895753b6780f26
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2023-11-23 23:51:52 +0100

    Close sidebar button

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -9,7 +9,11 @@ export class SideDrawer {
   // "Reflect" property reflect the attr change in the light dom HTML
   @Prop({ reflect: true }) sdTitle: string;
   // If we're only going to change some styles it isn't actually needed:
-  @Prop({ reflect: true }) open: boolean;
+  @Prop({ reflect: true, mutable: true }) open: boolean;
+
+  onCloseDrawer() {
+    this.open = false;
+  }
 
   render() {
     // // Easy but less powerful way:
@@ -18,6 +22,7 @@ export class SideDrawer {
     return (
       <aside>
         <header>
+          <button onClick={this.onCloseDrawer.bind(this)}>X</button>
           <p>{this.sdTitle}</p>
         </header>
         <main>
