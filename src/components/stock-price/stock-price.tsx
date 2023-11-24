5ab2bbafa32ac030d071123683bd2813e4625284
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2023-11-24 19:35:10 +0100

    Fetch price

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -1,23 +1,31 @@
-import { Component, h } from '@stencil/core';
+import { Component, State, h } from '@stencil/core';
 
 @Component({
   tag: 'stock-price',
   styleUrl: 'stock-price.css',
 })
 export class StockPrice {
-  fetchStockPrice(event: Event) {
+  @State() price = 0;
+  async fetchStockPrice(event: Event) {
     event.preventDefault();
     console.log('submit');
+    try {
+      const response = await fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=demo');
+      const result = await response.json();
+      this.price = result['Global Quote']['05. price'];
+    } catch (err) {
+      console.log(err);
+    }
   }
   render() {
     return [
-      <form onSubmit={this.fetchStockPrice}>
+      <form onSubmit={this.fetchStockPrice.bind(this)}>
         <input type="text" id="stock-symbol" />
         <button type="submit">Fetch</button>
       </form>,
 
       <div>
-        <p>Price: {0}</p>
+        <p>Price: {this.price}</p>
       </div>,
     ];
   }
